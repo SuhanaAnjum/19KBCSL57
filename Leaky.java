60138c8df24e5670ccaf62b708d2d26e442bbef1
Suhana Anjum <97942862+SuhanaAnjum@users.noreply.github.com>
2023-02-05 17:41:32 +0530

    fix: var names

--- Leaky.java
+++ Leaky.java
@@ -3,45 +3,44 @@ import java.util.Scanner;
 public class Leaky {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
-        int no_groups, bucket_size;
+        int num_groups, bucket_size;
 
         System.out.print("Enter the bucket size: ");
         bucket_size = sc.nextInt();
 
         System.out.print("Enter the number of groups: ");
-        no_groups = sc.nextInt();
+        num_groups = sc.nextInt();
 
-        int no_packets[] = new int[no_groups];
-        int in_bandwidth[] = new int[no_groups];
+        int num_packets[]  = new int[num_groups];
+        int in_bandwidth[] = new int[num_groups];
 
-        int out_bandwidth;
         int required_bandwidth = 0;
         int total_packets = 0;
 
-        for (int i = 0; i < no_groups; i++) {
+        for (int i = 0; i < num_groups; i++) {
             System.out.print("Enter the number of packets for group " + (i + 1) + ": ");
-            no_packets[i] = sc.nextInt();
+            num_packets[i] = sc.nextInt();
 
-            System.out.print("Enter the input bandwidth for the group " + (i + 1) + ": ");
+            System.out.print("Enter the input   bandwidth for group " + (i + 1) + ": ");
             in_bandwidth[i] = sc.nextInt();
 
-            if (total_packets + no_packets[i] <= bucket_size)
-                total_packets += no_packets[i];
+            if (total_packets + num_packets[i] <= bucket_size)
+                total_packets += num_packets[i];
             else {
                 do {
                     System.out.println("Bucket overflow");
                     System.out.print("Enter value less than " + (bucket_size - total_packets) + ": ");
-                    no_packets[i] = sc.nextInt();
-                } while (total_packets + no_packets[i] > bucket_size);
+                    num_packets[i] = sc.nextInt();
+                } while (total_packets + num_packets[i] > bucket_size);
 
-                total_packets += no_packets[i];
+                total_packets += num_packets[i];
             }
-            required_bandwidth += (no_packets[i] * in_bandwidth[i]);
+            required_bandwidth += (num_packets[i] * in_bandwidth[i]);
         }
 
         System.out.println("The total required bandwidth is: " + required_bandwidth);
         System.out.print("Enter the output bandwidth: ");
-        out_bandwidth = sc.nextInt();
+        int out_bandwidth = sc.nextInt();
 
         int temp = required_bandwidth;
         int remaining_packets = total_packets;
@@ -62,9 +61,9 @@ public class Leaky {
  *  Enter the bucket size: 3
  *  Enter the number of groups: 2
  *  Enter the number of packets for group 1: 1
- *  Enter the input bandwidth for the group 1: 2
+ *  Enter the input   bandwidth for group 1: 2
  *  Enter the number of packets for group 2: 3
- *  Enter the input bandwidth for the group 2: 1
+ *  Enter the input   bandwidth for group 2: 1
  *  Bucket overflow
  *  Enter value less than 2: 1
  *  The total required bandwidth is: 3
